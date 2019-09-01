c81b7c51a07463afad7b33b9af336c39687410bd
kylinGo <593289488@qq.com>
2019-09-01 11:43:37 +0800

    ValidateWrapper

--- components/ValidateWrapper/index.jsx
+++ components/ValidateWrapper/index.jsx
@@ -8,28 +8,39 @@ import "./index.less";
 import React from "react";
 import PropTypes from "prop-types";
 import classnames from "classnames";
+import { Popover } from "antd";
 
 export default class ValidateWrapper extends React.Component {
 
   static propTypes = {
-    
+    validateStatus: PropTypes.object,
   }
 
   static defaultProps = {
-    
+    validateStatus: {},
   }
 
   render() {
     const { className } = this.props;
+    const { status, message } = this.props.validateStatus;
+    const isError = status === "error";
     return (
-      <div
-        className={classnames({
-          ["comp-validate-wrapper-wrapper"]: true,
-          [className]: !!className
+      <Popover
+        content={message && message[0]}
+        visible={isError}
+        overlayClassName={classnames({
+          [className]: !!className,
+          "popover-error": isError,
         })}
       >
-        ValidateWrapper
-      </div>
+        <div
+          className={classnames({
+            "field-error": isError
+          })}
+        >
+          {this.props.children}
+        </div>
+      </Popover>
     )
   }
 }
