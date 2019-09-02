ddf04086ffc4918e15dfc60749e3d3fe13a21f02
冉光宇 <593289488@qq.com>
2019-09-02 18:04:55 +0800

    HomePage 增加组件搜索功能

--- src/preview/HomePage/index.jsx
+++ src/preview/HomePage/index.jsx
@@ -7,6 +7,7 @@
 import "./index.less";
 import React from "react";
 import { render } from "react-dom";
+import { Input } from "antd";
 
 const req = require.context("@preview", true, /\.jsx$/);
 let navs = req.keys();
@@ -19,6 +20,11 @@ class Home extends React.Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      value: "",
+      list: navs,
+    }
   }
 
   componentDidMount() {
@@ -26,12 +32,32 @@ class Home extends React.Component {
   }
 
   render() {
+    let { value, list } = this.state; 
     return (
       <div className="page-home-wrapper">
         <h1>Preview All Components</h1>
         <hr />
+        <div style={{marginBottom: 10}}>
+          <Input
+            placeholder="组件搜索"
+            style={{width: 200}}
+            value={value}
+            onChange={(e) => {
+              let value = e.target.value;
+              if (value) {
+                list = list.filter(item => item.match(new RegExp(value, "i")));
+              } else {
+                list = navs;
+              }
+              this.setState({
+                value,
+                list,
+              });
+            }}
+          />
+        </div>
         <div>
-          {navs && navs.map((item, index) => {
+          {list && list.map((item, index) => {
             return (
               <div key={item}>{index + 1}、<a href={`/${item}.html`}>{item}</a></div>
             )
