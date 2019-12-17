a8ff09fccaff108ffe68634cf3c6adb6865b61e0
ranguangyu <593289488@qq.com>
2019-12-17 15:54:18 +0800

    fix(<HzTable>): <修改常量OPERATE_SPAN值>

--- components/HzTable/constant.js
+++ components/HzTable/constant.js
@@ -6,7 +6,7 @@
 
 export const EMPTY_CELL = "--"; // 无数据单元格占位符
 
-export const OPERATE_SPAN = 10; // 操作列按钮之前的间隔
+export const OPERATE_SPAN = 16; // 操作列按钮之前的间隔
 
 export const VALIDATE_TIPS_TYPE_NORMAL = 1; // ValidateWrapper 组件提示类型 normal
 
